81cc78240663c0ec73255ffe57efa8e1d5ade41f
BrendaPaiao <brendamelop8@gmail.com>
2026-01-05 09:48:32 -0300

    feat: configura estrutura inicial do server e client

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-import React from React
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+server.listen(5000, function () {
+    console.log("Server rodando...");
+});
