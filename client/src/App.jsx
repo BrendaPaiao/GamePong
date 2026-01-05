612991345974a101115a0a107f81cace852fbb4f
BrendaPaiao <brendamelop8@gmail.com>
2026-01-05 11:20:47 -0300

    fix: remove submódulo e passa a rastrear o client como pasta

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Pong from './components/pong';
+
+function App() {
+  return (
+    <div className="App">
+      <Pong></Pong>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/pong.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Pong = function () {
+    return (<div>Pong</div>);
+};
+
+export default Pong;
