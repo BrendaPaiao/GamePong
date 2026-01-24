2d28394da5a6ec8507bb9c95b937a7bc662ea3c0
BrendaPaiao <brendamelop8@gmail.com>
2026-01-23 23:42:37 -0300

    test: exemplo de renderização de players

--- client/src/components/pong.jsx
+++ client/src/components/pong.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 const Pong = function () {
-    return (<div>Pong</div>);
+
+    const players = {
+        player1: {
+            name: 'Player 1'
+        },
+        player2: {
+            name: 'Player 2'
+        }
+    };
+
+    return (
+        <div>
+            {Object.keys(players).map((key) => (
+                <div>{players[key].name}</div>
+            ))}
+        </div>
+    );
 };
 
 export default Pong;
---
+++ estudo.md
@@ -0,0 +1,8 @@
+## Commit: ...
+É feito um exemplo de como será, apenas para demonstração, a listagem de players.
+Então ele cria um objeto com o nome de *players*, com 2 players e seus repectivos nomes. Em seguida, ele pega as keys (posições) do objeto, utiliza a função *.map* para mapear todas as keys e devolve atráves de div's o nome de cada jogador pelas keys.
+Na tela aparece "Player 1
+                 Player 2".
+
+## Commit: ...
+# Socket.io
